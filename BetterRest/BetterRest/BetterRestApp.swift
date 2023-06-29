da10f87c135ac8bbdaf5fca92fbecd501d30a304
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-06-28 23:21:46 -0300

    BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Isaque da Silva on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Isaque da Silva on 28/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var amountSleepPerDay = 8.0
+    @State private var amountCupOfCoffee = 1
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    DatePicker("When do you want to wake up?", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .datePickerStyle(.wheel)
+                        .labelsHidden()
+                        .listRowBackground(Color(CGColor(red: 240, green: 240, blue: 246, alpha: 0)))
+                    
+                    Section{
+                        Stepper("\(amountSleepPerDay.formatted()) hours", value: $amountSleepPerDay, in: 4...12, step: 0.25)
+                    } header: {
+                        Text("Desired amount of sleep")
+                    }
+                    
+                    Section {
+                        Stepper("\(amountCupOfCoffee) Cup(s)", value: $amountCupOfCoffee, in: 1...20)
+                    } header: {
+                        Text("Daily Coffee intake")
+                    }
+                    
+                    Button("Cauculate", action: {
+                        
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .listRowBackground(Color(CGColor(red: 240, green: 240, blue: 246, alpha: 0)))
+                    .frame(width: 400)
+                }
+            }.navigationTitle("Better Rest")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
